d9afc40b72fb202c33ede50c5d42843143ca0e96
hykare <78385964+hykare@users.noreply.github.com>
2023-01-25 21:14:04 +0100

    create flight model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_090321) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_190900) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -22,4 +22,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_090321) do
     t.index ["code"], name: "index_airports_on_code", unique: true
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.integer "departure_airport_id"
+    t.integer "arrival_airport_id"
+    t.datetime "start"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
