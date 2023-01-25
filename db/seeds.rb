6841499c656c6440c43d3dc1ca834390e758ed32
hykare <78385964+hykare@users.noreply.github.com>
2023-01-25 21:09:37 +0100

    seed airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+require 'csv'
+
+airports = CSV.open('lib/airports.csv', headers: true, header_converters: :symbol)
+
+airports = airports.filter { |a| a[:iata_code] && (a[:type] == 'large_airport' || a[:type] == 'medium_airport') }
+
+puts airports.count
+
+airports.first(100).each do |a|
+  Airport.create code: a[:iata_code], name: a[:name], country_code: a[:iso_country], latitude: a[:latitude_deg],
+                 longitude: a[:longitude_deg]
+end
