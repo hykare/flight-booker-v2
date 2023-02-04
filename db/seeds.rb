d540a3c3e49d5dc2c62e8d8ff5bcb0d952994252
hykare <78385964+hykare@users.noreply.github.com>
2023-02-04 14:49:19 +0100

    add airport and flight route seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,26 @@
 
 require 'csv'
 
-airports = CSV.open('lib/airports.csv', headers: true, header_converters: :symbol)
-
-airports = airports.filter { |a| a[:iata_code] && (a[:type] == 'large_airport' || a[:type] == 'medium_airport') }
-
-puts airports.count
-
-airports.first(100).each do |a|
+# 4518 airports
+airports = CSV.open('lib/airports_medium_large.csv', headers: true, header_converters: :symbol)
+airports = airports.filter { |a| a[:iata_code] }
+airports.each do |a|
   Airport.create code: a[:iata_code], name: a[:name], country_code: a[:iso_country], latitude: a[:latitude_deg],
                  longitude: a[:longitude_deg]
 end
+
+# ~60k routes total
+routes = CSV.open('lib/routes.dat', headers: true, header_converters: :symbol)
+routes.first(100).each do |route|
+  departure_airport = Airport.find_by code: route[:departure_airport_iata]
+  arrival_airport = Airport.find_by code: route[:arrival_airport_iata]
+
+  next unless departure_airport && arrival_airport
+
+  3.times do |n|
+    Flight.create departure_airport:, arrival_airport:, start: Time.now + (n + 1).days, duration: 4.hours
+  end
+end
+
+print Flight.count
+puts ' flights'
