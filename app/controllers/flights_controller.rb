66641624db01b86270dce9ff725ed93c5cfdb8bd
hykare <78385964+hykare@users.noreply.github.com>
2023-02-05 22:08:09 +0100

    only show airports with flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,8 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = Airport.all.map { |a| [a.name, a.id] }
-    @dates = Flight.pluck(:start).map { |date| date.strftime("%d %b %Y") }.uniq
+    @departure_airport_options = Airport.with_scheduled_departures.alphabetical.map { |a| ["#{a.name} (#{a.code})", a.id] }
+    @arrival_airport_options = Airport.with_scheduled_arrivals.alphabetical.map { |a| ["#{a.name} (#{a.code})", a.id] }
+
+    @dates = Flight.pluck(:start).map { |date| date.strftime('%d %b %Y') }.uniq
   end
 end
--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,8 @@
 class Airport < ApplicationRecord
-  has_many :departing_flights, foreign_key: 'departure_airport', class_name: 'Flight'
-  has_many :arriving_flights, foreign_key: 'arrival_airport', class_name: 'Flight'
+  has_many :departing_flights, foreign_key: 'departure_airport_id', class_name: 'Flight'
+  has_many :arriving_flights, foreign_key: 'arrival_airport_id', class_name: 'Flight'
+
+  scope :alphabetical, -> { order(name: :asc) }
+  scope :with_scheduled_arrivals, -> { distinct.joins(:arriving_flights) }
+  scope :with_scheduled_departures, -> { distinct.joins(:departing_flights) }
 end
