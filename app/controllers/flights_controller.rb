cb679f2ae1d17beecf41df7315ea2d03daa68da8
hykare <78385964+hykare@users.noreply.github.com>
2023-02-05 20:27:32 +0100

    show available dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| [a.name, a.id] }
+    @dates = Flight.pluck(:start).map { |date| date.strftime("%d %b %Y") }.uniq
   end
 end
