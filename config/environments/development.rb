cc91308cec31557772d70197de9f18b95ad272ed
hykare <78385964+hykare@users.noreply.github.com>
2023-02-10 20:48:50 +0100

    allow ngrok hosts for testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # ngrok tunneling thing
+  config.hosts << /[a-z0-9-.]+\.ngrok\.io/
 end
