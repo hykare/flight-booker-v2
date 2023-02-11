f82336ff03bb4483feab6e9758381dd3fb3f8fbc
hykare <78385964+hykare@users.noreply.github.com>
2023-02-11 15:50:00 +0100

    seed only flights in Europe

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,26 @@ require 'csv'
 airports = CSV.open('lib/airports_medium_large.csv', headers: true, header_converters: :symbol)
 airports = airports.filter { |a| a[:iata_code] }
 airports.each do |a|
-  Airport.create code: a[:iata_code], name: a[:municipality], country_code: a[:iso_country], latitude: a[:latitude_deg],
+  Airport.create code: a[:iata_code], name: a[:municipality], region: a[:continent], country_code: a[:iso_country], latitude: a[:latitude_deg],
                  longitude: a[:longitude_deg]
 end
 
+start_time = Time.now
+
 # ~60k routes total
-routes = CSV.open('lib/routes.dat', headers: true, header_converters: :symbol)
-routes.first(100).each do |route|
+# 14695 in EU
+routes = CSV.open('lib/routes_EU.dat', headers: true, header_converters: :symbol)
+routes.each do |route|
   departure_airport = Airport.find_by code: route[:departure_airport_iata]
   arrival_airport = Airport.find_by code: route[:arrival_airport_iata]
 
   next unless departure_airport && arrival_airport
 
-  3.times do |n|
+  rand(3..5).times do |n|
     Flight.create departure_airport:, arrival_airport:, start: Time.now + (n + 1).days, duration: 4.hours
   end
 end
 
+puts Time.now - start_time
 print Flight.count
 puts ' flights'
